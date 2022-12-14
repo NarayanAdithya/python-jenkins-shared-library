805b2a950e01b4f58e4d13af300048ccbaffb04c
NarayanAdithya <narayanadithya1234@gmail.com>
2022-12-14 21:29:37 +0530

    Version Retrieval Update

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -4,15 +4,15 @@ package com.example
 
 class Docker implements Serializable {
     def script
-
+    def Version
     Docker(script){
         this.script = script
     }
     def buildDockerImage(){
         script.withCredentials([script.usernamePassword(credentialsId:'dockerhub-adina', passwordVariable: 'PASS', usernameVariable: 'USER')]){
             def matcher = script.readFile('version.toml') =~ ".\\..\\.."
-            Version = matcher[0]
-            script.echo "$Version"
+            this.Version = matcher[0]
+            script.echo "$this.Version"
         }
     }
 }
